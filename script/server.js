9526df0552062768cc7a1cfe7de041998a06aa83
xianpei <xianpei.liu@dingjust.com>
2019-05-16 17:43:52 +0800

    视差滚动

--- script/server.js
+++ script/server.js
@@ -3,7 +3,9 @@ const Bundler = require('parcel-bundler')
 const express = require('express')
 
 const app = express()
-const bundler = new Bundler('src/*.html', { outDir: './bin' })
+const bundler = new Bundler('src/*.html', {
+  outDir: './bin'
+})
 
 /**
  * 代理设置
@@ -15,5 +17,5 @@ app.use('/api', proxy({ target: 'http://localhost:3000'}))
  */
 app.use(bundler.middleware())
 const port = Number(process.env.PORT || 7878)
-console.log('[linsten]', `http://localhost:${port}`)
+console.log('[listen]', `http://localhost:${port}`)
 app.listen(port)
--- src/app.vue
+++ src/app.vue
@@ -1,13 +1,90 @@
 <template>
-  <div class="vue-app"> VUE APP TEST</div>
+  <div class="vue-app"> 
+    	<div id="content-wrapper">
+        <div id="example-wrapper">
+          <div class="scrollContent">
+            <section id="titlechart">
+              <div id="description">
+                <h1 class="badge gsap">视差滚动</h1>
+                <h2>The most common parallax application</h2>
+                <p>
+                  To create a classic parallax page effect simply add elements that contain the parallax background.<br>
+                  Then use scenes to animate the child element's position.
+                </p>
+                <p>
+                  The DOM is very straight forward and consists of consecutive containers.
+                </p>
+                <a href="#" class="viewsource">view source</a>
+              </div>
+            </section>
+            <section class="demo">
+              <div class="spacer s0"></div>
+              <div id="parallax1" class="parallaxParent">
+                <div></div>
+              </div>
+              <div class="spacer s1">
+                <div class="box2 blue">
+                  <p>Content 1</p>
+                  <a href="#" class="viewsource">view source</a>
+                </div>
+              </div>
+              <div class="spacer s0"></div>
+              <div id="parallax2" class="parallaxParent">
+                <div></div>
+              </div>
+              <div class="spacer s1">
+                <div class="box2 blue">
+                  <p>Content 2</p>
+                  <a href="#" class="viewsource">view source</a>
+                </div>
+              </div>
+              <div class="spacer s0"></div>
+              <div id="parallax3" class="parallaxParent">
+                <div></div>
+              </div>
+              <div class="spacer s2"></div>
+            </section>
+            <div class="spacer s_viewport"></div>
+          </div>
+        </div>
+      </div>
+  </div>
 </template>
 <script>
+import 'gsap/src/uncompressed/TweenMax'
+window.ScrollMagic = require('scrollmagic')  // 正确的引入方式，需要赋给全局
+import 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap'
+import 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators'
+
 export default {
   name: 'App',
   data() {
     return {
-
+      controller: null
     }
+  },
+  methods: {
+    createScene() {
+      new ScrollMagic.Scene({triggerElement: "#parallax1"})
+        .setTween("#parallax1 > div", {y: "80%", ease: Linear.easeNone})
+        .addIndicators()
+        .addTo(this.controller);
+
+      new ScrollMagic.Scene({triggerElement: "#parallax2"})
+        .setTween("#parallax2 > div", {y: "80%", ease: Linear.easeNone})
+        .addIndicators()
+        .addTo(this.controller);
+
+      new ScrollMagic.Scene({triggerElement: "#parallax3"})
+        .setTween("#parallax3 > div", {y: "80%", ease: Linear.easeNone})
+        .addIndicators()
+        .addTo(this.controller);
+    }
+  },
+  mounted() {
+    this.controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}})
+    this.createScene()
+    console.log('controller', this.controller)
   }
 }
 </script>
